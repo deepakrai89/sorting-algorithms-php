e7ed21160fa64292f1f9152aa1db875ebbef4900
Deepak Rai <deepakr@mindfiresolutions.com>
2017-07-29 23:07:54 +0530

    Updated script to show time  taken by script to execute.

--- selection_sort.php
+++ selection_sort.php
@@ -18,11 +18,14 @@ $array[$i_min] = $temp;
 
 return $array;
 }
-
+$start_time = microtime(true);
 $unsorted_array = array(2,4,3,7,9,1,5,6,8);
 $count = count($unsorted_array)-1;
 $sorted_array = selection_sort($unsorted_array, $count);
-//$sorted_array = selection _sort ($unsorted_array,count($unsorted_array));
-print_r($sorted_array);exit;
+
+$end_time = microtime(true);
+
+print_r($sorted_array);
+echo 'executed in'.number_format(($end_time-$start_time),3); ;
 
 ?>
